00c6253e9be9a56c01d0f96fecfa477c2c8386d5
diegher-testing <96258518+diegher-testing@users.noreply.github.com>
2023-04-11 18:39:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,34 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.46.0"
+      version = "~> 3.0.2"
     }
   }
 
-  required_version = ">= 1.3.9"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  use_oidc            = true
-  storage_use_azuread = true
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  virtual_network_name = azurerm_virtual_network.example.name
-  resource_group_name  = azurerm_resource_group.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
